d6715b24e18ff2c34a33c5486bc0832e05631a1a
roomey_rahman <roomeyrahman@gmail.com>
2025-01-14 18:27:50 +0600

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpErrorFilter } from './common/filters';
 import { LoggingInterceptor, TransformInterceptor } from './common/interceptor';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config';
+
+
+const DB_CONNECTION = process.env.DB_CONNECTION;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.SECRET_KEY_JWT,
+    }),
+    MongooseModule.forRoot(DB_CONNECTION, {
+      autoIndex: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
